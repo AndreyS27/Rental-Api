834afbee5b37d396e47c118b386d71c1cb6265a9
Andrey S <salihov.andrey1@yandex.ru>
2025-06-04 15:27:27 +0300

    Added Apartment Controller (REST)

---
+++ Controllers/ApartmentController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("[controller]")]
+public class ApartmentController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ApartmentController(AppDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _context.Apartments.Include(a => a.Owner).ToListAsync());
+    }
+}
