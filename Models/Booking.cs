824caeddf21184fca7db272e5cb83d266b98128e
Andrey S <salihov.andrey1@yandex.ru>
2025-06-04 14:11:23 +0300

    Added Booking model

---
+++ Models/Booking.cs
@@ -0,0 +1,8 @@
+public class Booking
+{
+    public int Id { get; set; }
+    public int ApartmentId { get; set; }
+    public int UserId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
